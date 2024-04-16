5703dd65e392a6111f5c388d2886e195fb739943
RedonMehmeti <redonmehmeti2019@gmail.com>
2024-04-16 09:06:56 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function generateQRCode(){
+    var inputText = document.getElementById("inputText").value;
+    var linku = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example " +(inputText) + "&bgcolor=ffff00";
+    document.getElementById("fotoja").src = linku;
+}
